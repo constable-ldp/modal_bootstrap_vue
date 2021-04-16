4a476707b8ea6a474924755a4bb8d3866583ced1
Luke <constableldp@gmail.com>
2021-04-16 12:07:20 +0100

    Inital commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-button @click="showModal">Open</b-button>
+    <b-modal ref="my-modal">
+        <h3>Modal!</h3>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      showModal() {
+        this.$refs['my-modal'].show()
+      }
+    }
+  }
+</script>
